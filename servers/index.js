ff1a3bebc6002cfa16bff03636de04709036ebf7
wuhongye0512 <1041941520@qq.com>
2023-10-05 22:33:26 +0800

    test

--- servers/index.js
+++ servers/index.js
@@ -2,7 +2,7 @@
  * @Author: hongye.wu hongye.wu@dounion.com
  * @Date: 2023-08-05 09:38:12
  * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2023-10-05 22:28:14
+ * @LastEditTime: 2023-10-05 22:33:12
  * @FilePath: /node-server/create-schema-tool/servers/index.js
  * @Description:
  *
@@ -41,7 +41,7 @@ router.post('/sendFile', (req, res) => {
   //设置文件大小限制
   // form.maxFilesSize = 1 * 1024 * 1024;
   form.parse(req, function (err, fields, files) {
-    console.log(process.env.NODE_ENV, 'err, fields, files');
+    console.log(process.env.NODE_ENV,err, 'err, fields, files');
     try {
       let inputFile = files.file[0];
       let uploadedPath = inputFile.path;
